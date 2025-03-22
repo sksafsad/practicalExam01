29196c7b3c9a57d909d66bef2ee39c0404563419
sksafsad <s.safwans11@gmail.com>
2025-03-22 17:48:36 +0600

    Fifth  Draft

--- jscodes.js
+++ jscodes.js
@@ -1,3 +1,8 @@
 function addToCart(productName) {
     alert(productName + ' added to cart!');
 }
+
+function contactSubmit(){
+    var name1= document.getElementById("name").value;
+    alert("Thanks for your input "+ name1+", we will contact you shortly.")
+}
